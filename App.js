27265dd03e8c6aff9fcfda19f302ed49cff58e5e
sutsko <44364186+sutsko@users.noreply.github.com>
2019-12-18 18:09:23 +0100

    Adding to previous commit

--- App.js
+++ App.js
@@ -11,22 +11,7 @@ import {Block} from './components';
 //import all used images
 const images = [
   require('./assets/icons/back.png'),
-  require('./assets/icons/plants.png'),
-  require('./assets/icons/seeds.png'),
-  require('./assets/icons/flowers.png'),
-  require('./assets/icons/sprayers.png'),
-  require('./assets/icons/pots.png'),
-  require('./assets/icons/fertilizers.png'),
-  require('./assets/images/plants_1.png'),
-  require('./assets/images/plants_2.png'),
-  require('./assets/images/plants_3.png'),
-  require('./assets/images/explore_1.png'),
-  require('./assets/images/explore_2.png'),
-  require('./assets/images/explore_3.png'),
-  require('./assets/images/explore_4.png'),
-  require('./assets/images/explore_5.png'),
-  require('./assets/images/explore_6.png'),
-  require('./assets/images/avatar.png'),
+  //Can be used as caching mechanism 
 ];
 
 export default class App extends React.Component{
