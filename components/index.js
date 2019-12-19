17f4735db26651817301316ba2a0a0299ffd58d7
sutsko <44364186+sutsko@users.noreply.github.com>
2019-12-19 09:02:38 +0100

    Slettede badge og course

--- components/index.js
+++ components/index.js
@@ -1,5 +1,4 @@
 import Block from "./Block";
-import Badge from "./Badge";
 import Button from "./Button";
 import Card from "./Card";
 import Input from "./Input";
@@ -7,8 +6,7 @@ import Text from "./Text";
 import Progress from "./Progress";
 import Divider from "./Divider";
 import Switch from "./Switch";
-import Course from "./Course";
 import RecommendedCardItem from "./RecommendedCardItem";
 import Panel from "./Panel";
 
-export { Block, Badge, Button, Card, Input, Text, Progress, Divider, Switch, Course, RecommendedCardItem, Panel };
+export { Block, Button, Card, Input, Text, Progress, Divider, Switch, RecommendedCardItem, Panel };
